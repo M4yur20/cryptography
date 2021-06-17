560baf33d83d1c108d97834890cd54e423d3d88b
Mayur Agarwal <mayuragarwal929@gmail.com>
2021-06-17 16:21:43 +0530

    done

--- RSA-based-chat-application-master/RSA/settings.py
+++ RSA-based-chat-application-master/RSA/settings.py
@@ -73,12 +73,8 @@ WSGI_APPLICATION = 'RSA.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bravo_cryptography',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': 3306,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
